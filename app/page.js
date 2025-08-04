0005d2ba377bfb8fd11d720220f29fae4e596325
Vikas <vikas1000dhiman@outlook.com>
2025-08-04 19:49:13 +0530

    Built basic layout of the webpage

--- app/page.js
+++ app/page.js
@@ -1,103 +1,212 @@
-import Image from "next/image";
+import Image from 'next/image'
+import logo from '@/public/logo1.png'
 
-export default function Home() {
+export default async function Home() {
+  const aboutUsData = [
+    'At The Magic of Mom, we believe that every meal should feel like home. Our mission is to deliver fresh, hygienic, and lovingly prepared tiffins that bring the warmth of a mother’s kitchen to your doorstep.',
+    "Whether you're a student living away from home, a working professional with a busy schedule, or a senior citizen in need of nutritious meals — we’ve got you covered with customizable plans and on-time delivery.",
+    'With a focus on quality ingredients, eco-friendly packaging, and personal service through WhatsApp and social platforms, we’re redefining convenience without compromising on love and care.',
+  ]
+
+  const testimonialsCard = [
+    {
+      title: 'Ritika, MBA Student',
+      discription:
+        'I used to skip meals during hectic classes, but Mom’s Tiffin changed that. Nutritious, tasty, and always on time!',
+    },
+    {
+      title: 'Ankit, Software Engineer',
+      discription:
+        'Feels just like home-cooked food. The subscription is flexible, and I never miss lunch even on busy days.',
+    },
+  ]
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className='lg:pl-0 flex flex-col md:flex-row md:justify-between items-center relative sm:pl-[50px] mt-10 lg:mt-28 gap-7 md:gap-0 md:mb-4 mb-4'>
+        <div className='flex flex-col items-center md:items-start gap-7'>
+          <div className='w-full text-center md:text-left max-w-[46rem] overflow-hidden text-4xl sm:text-5xl lg:text-[87px] text-[#331B3B] font-bold font-hindVadodara leading-[99.8%] tracking-[0.01]'>
+            <h1 className='whitespace-nowrap'>We serve homely</h1>
+            <p />
+            <span className='font-light'>
+              meals with
+              <br />
+              <span className='relative w-full z-10'>
+                Mom’s Touch
+                <span className='bg-[#FDC221] lg:h-5 h-2 sm:bottom-2.5 bottom-1.5 lg:bottom-4 -z-[1] left-0 absolute w-full' />
+              </span>
+            </span>
+          </div>
+          <div className='max-w-[515px] flex flex-col gap-8'>
+            <span className='text-[17px] font-normal font-hindVadodara leading-[146.3%] tracking-wide text-center md:text-left lg:mt-4 md:mt-3 mt-2'>
+              Busy days shouldn't mean unhealthy meals. Our tiffins bring
+              home-style flavors, crafted fresh and delivered with love—just
+              like Mom makes.
+            </span>
+            <button className='self-center md:self-start max-w-[207px] py-[22px] w-full flex justify-center items-center font-bold leading-[137.3%] tracking-wide bg-[#331B3B] text-white rounded-md lg:mt-4 md:mt-3 mt-2'>
+              Start Subscriptions
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={logo}
+            alt='Home'
+            className='h-[362px] lg:h-full p-2'
+            width={450}
+            height={450}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-5 justify-start items-center md:items-start'>
+        <div className='flex flex-col relative md:mt-28 lg:text-5xl text-4xl text-[#331B3B] leading-[106.3%] -tracking-[0.01] ont-hindVadodara'>
+          <h1 className=' font-bold text-center md:text-left'>About Us</h1>
+          <span className='lg:text-5xl text-4xl font-light'>
+            Fake or
+            <span className='ml-3 relative z-10'>
+              Real
+              <span className='bg-[#FDC221] h-2 bottom-[10px] lg:bottom-[14px] -z-[1] left-0 absolute w-full' />
+            </span>
+          </span>
+        </div>
+        <div className='flex flex-col gap-2 p-5'>
+          {aboutUsData.map((data, index) => {
+            return (
+              <span
+                className='text-[15px] mt-[6px] max-w-[515px] text-center md:text-left Light font-light font-hindVadodara leading-[145.3%] tracking-[0.02]'
+                key={index}
+              >
+                {data}
+              </span>
+            )
+          })}
+        </div>
+        <div className='mt-2 md:mt-10'>
+          <button className='self-center md:self-start max-w-[207px] py-[18px] lg:px-5 w-full flex justify-center items-center font-bold leading-[137.3%] tracking-wide bg-[#331B3B] text-white rounded-md px-5'>
+            Explore More
+          </button>
+        </div>
+      </div>
+
+      {/* Subscriptions Section */}
+      <div className='mt-7 mx-auto'>
+        <div className='mb-7 max-w-2xl text-center mx-auto'>
+          <h2 className='md:text-5xl text-center w-full text-3xl  text-[#331B3B] font-bold'>
+            <span className='relative z-10 ml-2'>
+              Subscriptions
+              <span className='bg-[#FDC221] h-2 md:bottom-2 bottom-1 -z-[1] absolute w-full right-[100px] sm:right-0 left-0' />
+            </span>
+          </h2>
+        </div>
+
+        <div className='flex lg:min-h-[34rem] min-h-[97rem] pt-[30px] px-[40px] bg-[#331B3B] mb-3 rounded-[10px] mx-2'>
+          <div className='min-w-full'>
+            <div className=' grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px]'>
+              {[1, 2, 3].map((id) => (
+                <div
+                  key={id}
+                  className='w-full bg-white rounded-[10px] shadow border border-[#E1E3E5] divide-y'
+                >
+                  <div className='pt-[15px] px-[25px] pb-[25px]'>
+                    <div className='flex justify-end'>
+                      <div className='bg-[#F6F6F7] rounded-[20px] flex justify-center items-center px-[12px]'>
+                        <p className='text-[#00153B] text-[12px] leading-[28px] font-bold'>
+                          {id === 1 ? 'Starter' : id === 2 ? 'Value' : 'Pro'}
+                        </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p className='text-[#00153B] text-[19px] font-bold'>
+                        {id === 1
+                          ? 'Trial'
+                          : id === 2
+                          ? 'Fast Start'
+                          : 'Accelerate'}
+                      </p>
+                      <p className='text-[#00153B] text-[50px] font-bold'>
+                        {id === 1 ? 'Free' : id === 2 ? '$49' : '$89'}
+                      </p>
+                    </div>
+                    <div>
+                      <p className='text-[#717F87] text-[18px] font-medium'>
+                        {id === 1
+                          ? '5 Credits'
+                          : id === 2
+                          ? '50 Credits per month'
+                          : '100 Credits per month'}
+                      </p>
+                      <p className='text-[#717F87] text-[18px] font-medium'>
+                        {id === 1 ? '1 User' : 'Unlimited users'}
+                      </p>
+                    </div>
+                  </div>
+                  <div className='pt-[25px] px-[25px] pb-[35px]'>
+                    <div className='text-[#717F87] text-[14px] font-medium space-y-1'>
+                      <p>Freshly cooked home-style meals</p>
+                      <p>Hygienic packaging & prompt delivery</p>
+                      <p>Customizable daily menu</p>
+                      <p>Ideal for students & professionals</p>
+                      <p>WhatsApp support & reminders</p>
+                    </div>
+                    <div className='mt-[25px]'>
+                      <button
+                        className={`${
+                          id === 2
+                            ? 'bg-[#E1E3E5] text-black'
+                            : 'bg-[#006EF5] text-white'
+                        } rounded-[5px] py-[15px] px-[25px] text-[14px] font-semibold`}
+                      >
+                        {id === 2
+                          ? 'Current Plan'
+                          : id === 1
+                          ? 'Downgrade +'
+                          : 'Upgrade +'}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Testimonials Section */}
+        <div className='flex flex-col items-center justify-center w-full gap-14 md:mt-[75px] mt-9 px-5'>
+          <div className='text-center w-full text-3xl md:text-5xl text-[#331B3B] font-hindVadodara -tracking-[0.01em]'>
+            <h1 className='whitespace-nowrap font-bold leading-[99.8%] text-center'>
+              Our Testimonials
+            </h1>
+            <span className='md:whitespace-nowrap !text-center font-light mt-1 max-w-sm md:max-w-xl mx-auto'>
+              What Our Clients Are
+              <span className='relative z-10 ml-2'>
+                Saying
+                <span className='bg-[#FDC221] h-2 md:bottom-2 bottom-1 -z-[1] absolute w-full right-[100px] sm:right-0 left-0' />
+              </span>
+            </span>
+          </div>
+
+          <div className='flex sm:gap-[69px] gap-7 flex-col md:flex-row mb-6'>
+            {testimonialsCard.map((data, index) => (
+              <div
+                key={index}
+                className='flex flex-col gap-7 justify-start drop-shadow-[0_7px_9px_rgba(0,0,0,0.15)] bg-white pl-[29px] pr-[35px] py-9'
+              >
+                <div>{/* SVG icon */}</div>
+                <div className='flex flex-col justify-start gap-3'>
+                  <span className='max-w-[384px] w-full text-base font-normal font-hindVadodara leading-[159.8%] tracking-wide'>
+                    {data.discription}
+                  </span>
+                  <span className='flex gap-2 items-center text-base font-semibold font-hindVadodara leading-[159.8%] tracking-wide'>
+                    <span className='border-[2px] border-[#FDC221] w-3 relative bottom-0.5' />
+                    {data.title}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
---
+++ component/Navbar.js
@@ -0,0 +1,152 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
+import '@/app/globals.css'
+import Link from 'next/link'
+import logo from '@/public/trend.svg'
+import Image from 'next/image'
+
+const Navbar = () => {
+  // Fetch number of item in user cart
+
+  const [toggle, setToggle] = useState(false)
+
+  //For small screen navigation
+  const handelToggle = () => {
+    setToggle(!toggle)
+  }
+
+  //To get current path such that we can change our Link color when it is active
+  const pathname = usePathname()
+
+  // To prevent hyderation cause due to data present in local storage
+
+  return (
+    <header className='flex   px-4 sm:pl-2  sm:pr-10 bg-white  min-h-[70px] tracking-wide relative z-50 '>
+      <div className='flex flex-wrap items-center justify-between gap-5 w-full'>
+        <Link href='/'>
+          <Image src={logo} width={180} height={80} alt='Logo' priority />
+        </Link>
+
+        <div
+          id='collapseMenu'
+          className={`${
+            toggle ? 'block' : 'hidden'
+          } lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
+        >
+          <button
+            id='toggleClose'
+            onClick={handelToggle}
+            className='lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='w-4 fill-black'
+              viewBox='0 0 320.591 320.591'
+            >
+              <path
+                d='M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z'
+                data-original='#000000'
+              ></path>
+              <path
+                d='M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z'
+                data-original='#000000'
+              ></path>
+            </svg>
+          </button>
+
+          <ul className='lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
+            <li className='mb-6 hidden max-lg:block'>
+              <Link href='/'></Link>
+            </li>
+            <li className='max-lg:border-b max-lg:py-3 px-4'>
+              <Link
+                href='/'
+                className={`${
+                  pathname === '/' ? 'text-blue-700 font-bold' : 'text-gray-700'
+                } hover:border-b-2 border-gray-600 duration-100`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className='max-lg:border-b max-lg:py-3 px-4'>
+              <Link
+                href='/products'
+                className={`${
+                  pathname === '/products'
+                    ? 'text-blue-700 font-bold'
+                    : 'text-gray-700'
+                } hover:border-b-2 border-gray-600 duration-100`}
+              >
+                Products
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex items-center max-lg:ml-auto space-x-5'>
+          <span
+            className={`relative ${
+              pathname === '/cart' ? 'text-blue-700 font-bold' : 'text-gray-700'
+            } hover:text-blue-700`}
+          >
+            <Link
+              href='/cart'
+              className={`${
+                pathname === '/cart'
+                  ? 'text-blue-700 font-bold'
+                  : 'text-gray-700'
+              } hover:border-b-2 border-gray-600 duration-100`}
+            >
+              Cart
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='20px'
+                height='20px'
+                className={` cursor-pointer ${
+                  pathname === '/cart'
+                    ? 'fill-[#1d4ed8] inline'
+                    : 'text-gray-700'
+                } hover:fill-[#1d4ed8] inline`}
+                viewBox='0 0 512 512'
+              >
+                <path
+                  d='M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0'
+                  data-original='#000000'
+                ></path>
+              </svg>
+            </Link>
+            <span
+              className={` absolute left-auto -ml-1 top-0 rounded-full px-1 py-0 text-xs text-white ${
+                pathname === '/cart' ? 'bg-blue-700 ' : 'bg-slate-700'
+              } hover:bg-blue-700`}
+            >
+              {0}
+            </span>
+          </span>
+
+          <button
+            id='toggleOpen'
+            className='lg:hidden !ml-7'
+            onClick={handelToggle}
+          >
+            <svg
+              className='w-7 h-7'
+              fill='#000'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fillRule='evenodd'
+                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+                clipRule='evenodd'
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
